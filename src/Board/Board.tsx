f4d2d222335da0c0fbb2a117bf703f2b001b966e
Ilia Lebedev <ilia@neon.tech>
2025-05-16 11:33:17 +0200

    gameplay logic

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -12,17 +12,20 @@ const OVERLAPPING_THRESHOLD = "60%";
 
 type GameState = {
   tiles: DeckTile[];
+  onBoardTiles: TileId[];
   hand: TileId[];
 };
 
 export const Board = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
+  const initialZone = useRef<HTMLDivElement>(null);
   const leftZone = useRef<HTMLDivElement>(null);
   const rightZone = useRef<HTMLDivElement>(null);
 
   const [gameState] = useState<GameState>(() => ({
     tiles: generateRandomizedDeck(),
+    onBoardTiles: [],
     hand: [],
   }));
 
@@ -49,10 +52,58 @@ export const Board = () => {
     });
   }
 
+  function actualizeOnBoardTilesPosition() {
+    if (gameState.onBoardTiles.length === 1) {
+      gameState.onBoardTiles.forEach((tileId) => {
+        gsap.to(tileSelector(tileId), {
+          x: 0,
+          y: 0,
+          rotate: 90,
+          duration: 0.15,
+        });
+      });
+    } else if (gameState.onBoardTiles.length === 2) {
+      gameState.onBoardTiles.forEach((tileId, index) => {
+        gsap.to(tileSelector(tileId), {
+          x: 94 * (index - 0.5),
+          y: 0,
+          rotate: 90,
+          duration: 0.15,
+        });
+      });
+    } else {
+      // TODO
+    }
+  }
+
+  function checkInitialHit(t: Draggable): boolean {
+    return (
+      gameState.onBoardTiles.length === 0 &&
+      t.hitTest(initialZone.current, OVERLAPPING_THRESHOLD)
+    );
+  }
+
+  function checkLeftHit(t: Draggable): boolean {
+    return (
+      gameState.onBoardTiles.length > 0 &&
+      t.hitTest(leftZone.current, OVERLAPPING_THRESHOLD)
+    );
+  }
+
+  function checkRightHit(t: Draggable): boolean {
+    return (
+      gameState.onBoardTiles.length > 0 &&
+      t.hitTest(rightZone.current, OVERLAPPING_THRESHOLD)
+    );
+  }
+
   useGSAP(
     // @ts-expect-error: arguments can be used in the future.
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     (context, contextSafe) => {
+      leftZone.current!.classList.add("hide");
+      rightZone.current!.classList.add("hide");
+
       for (const { tileId, rotated } of gameState.tiles) {
         gsap.to(tileSelector(tileId), {
           duration: 0,
@@ -80,17 +131,13 @@ export const Board = () => {
         onDrag() {
           const t = this as Draggable;
 
-          const isLeftOverlapped = t.hitTest(
-            leftZone.current,
-            OVERLAPPING_THRESHOLD,
-          );
-          leftZone.current!.classList.toggle("highlight", isLeftOverlapped);
+          const isInitialHit = checkInitialHit(t);
+          const isLeftHit = checkLeftHit(t);
+          const isRightHit = checkRightHit(t);
 
-          const isRightOverlapped = t.hitTest(
-            rightZone.current,
-            OVERLAPPING_THRESHOLD,
-          );
-          rightZone.current!.classList.toggle("highlight", isRightOverlapped);
+          initialZone.current!.classList.toggle("highlight", isInitialHit);
+          leftZone.current!.classList.toggle("highlight", isLeftHit);
+          rightZone.current!.classList.toggle("highlight", isRightHit);
         },
         onRelease() {
           const t = this as Draggable;
@@ -98,13 +145,11 @@ export const Board = () => {
           const index = gameState.hand.indexOf(tileId);
           const isInHand = index !== -1;
 
-          const isLeftHit = t.hitTest(leftZone.current, OVERLAPPING_THRESHOLD);
-          const isRightHit = t.hitTest(
-            rightZone.current,
-            OVERLAPPING_THRESHOLD,
-          );
+          const isInitialHit = checkInitialHit(t);
+          const isLeftHit = checkLeftHit(t);
+          const isRightHit = checkRightHit(t);
 
-          if (isLeftHit || isRightHit) {
+          if (isInitialHit || isLeftHit || isRightHit) {
             // Validate turn
             const tileIndex = gameState.tiles.findIndex(
               (tile) => tile.tileId === tileId,
@@ -114,6 +159,19 @@ export const Board = () => {
             if (isInHand) {
               gameState.hand.splice(index, 1);
               actualizeHandTilesPosition();
+            }
+
+            if (isLeftHit) {
+              gameState.onBoardTiles.unshift(tileId);
+            } else {
+              gameState.onBoardTiles.push(tileId);
+            }
+            actualizeOnBoardTilesPosition();
+
+            if (isInitialHit) {
+              initialZone.current!.classList.add("hide");
+              leftZone.current!.classList.remove("hide");
+              rightZone.current!.classList.remove("hide");
             }
           } else {
             if (!isInHand) {
@@ -158,6 +216,7 @@ export const Board = () => {
 
   return (
     <div ref={containerRef} className={styles.board}>
+      <div ref={initialZone} className={styles.dropZone}></div>
       <div ref={leftZone} className={styles.dropZone}></div>
       <div ref={rightZone} className={styles.dropZone}></div>
       {gameState.tiles.map(({ tileId, values }) => (
